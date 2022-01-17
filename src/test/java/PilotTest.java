410ca1b78f9a4d95b57780283aebdc6f2759b1bb
Martin Quinn <thefonz270@hotmail.com>
2022-01-17 09:38:54 +0000

    pilot test file fixed.

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,23 +1,24 @@
-//import junit.framework.TestCase;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class PilotTest {
-//
-//    private Pilot joker;
-//
-//    @Before
-//    public void before() {
-//         joker = new Pilot("Joker", CabinCrewRank.Pilot, "QE345");
-//    }
-//
-//    @Test
-//    public void pilotExists{
-//        assertEquals("Joker", joker.getName());
-//        assertEquals(CabinCrewRank.Pilot, joker.getRank());
-//        assertEquals("QE345", joker.getPilotLicenseNumber());
-//    }
-//
-//}
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PilotTest {
+
+    private Pilot joker;
+
+    @Before
+    public void before() {
+         joker = new Pilot("Joker", CabinCrewRank.Pilot, "QE345");
+    }
+
+    @Test
+    public void testPilot(){
+        assertEquals("Joker",joker.getName());
+        assertEquals(CabinCrewRank.Pilot, joker.getRank());
+        assertEquals("QE345", joker.getPilotLicenseNumber());
+    }
+
+
+}
